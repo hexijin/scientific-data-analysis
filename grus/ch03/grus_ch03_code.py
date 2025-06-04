aee252ea362d90269ca53719aa2d5dee5b317eaa
Brian Hill <brian@lingerhere.org>
2025-06-04 10:13:13 -0700

    Started working through Chapter 3 of Grus.

---
+++ grus/ch03/grus_ch03_code.py
@@ -0,0 +1,34 @@
+# p. 70 of PDF
+
+from matplotlib import pyplot as plt
+
+years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
+gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
+
+# create a line chart, years on x-axis, gdp on y-axis
+plt.plot(years, gdp, color='green', marker='o', linestyle='solid')
+
+# add a title
+plt.title("Nominal GDP")
+
+# add a label to the y-axis
+plt.ylabel("Billions of $")
+plt.show()
+
+# p. 72 of PDF
+
+movies = ["Annie Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
+num_oscars = [5, 11, 3, 8, 10]
+
+# plot bars with left x-coordinates [0, 1, 2, 3, 4], heights [num_oscars]
+plt.bar(range(len(movies)), num_oscars)
+plt.title("My Favorite Movies")    # add a title
+plt.ylabel("# of Academy Awards")  # label the y-axis
+
+# lable the x-axis with movie names at bar centers
+plt.xticks(range(len(movies)), movies)
+
+plt.show()
+
+# p. 73 of PDF
+
