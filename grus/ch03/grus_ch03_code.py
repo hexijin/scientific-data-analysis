9f7c476b52ca80c32d57de95d7da3c54173b83d1
Brian Hill <brian@lingerhere.org>
2025-06-04 11:16:46 -0700

    Finished Chapter 3 examples.

--- grus/ch03/grus_ch03_code.py
+++ grus/ch03/grus_ch03_code.py
@@ -1,6 +1,7 @@
 # p. 70 of PDF
 
 from matplotlib import pyplot as plt
+from collections import Counter
 
 years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
 gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
@@ -25,10 +26,99 @@ plt.bar(range(len(movies)), num_oscars)
 plt.title("My Favorite Movies")    # add a title
 plt.ylabel("# of Academy Awards")  # label the y-axis
 
-# lable the x-axis with movie names at bar centers
+# label the x-axis with movie names at bar centers
 plt.xticks(range(len(movies)), movies)
 
 plt.show()
 
 # p. 73 of PDF
 
+grades = [83, 95, 91, 87, 70, 0, 85, 82, 100, 67, 73, 77, 0]
+
+# Bucket grades by decile, but put 100 in with the 90s
+histogram = Counter(min(grade // 10 * 10, 90) for grade in grades)
+
+plt.bar([x + 5 for x in histogram.keys()],
+        histogram.values(),
+        10,
+        edgecolor=(0, 0, 0)
+        )
+
+plt.axis((-5, 105, 0, 5))
+
+plt.xticks([10 * i for i in range(11)])
+plt.xlabel("Decile")
+plt.ylabel("# of Students")
+plt.title("Distribution of Exam 1 Grades")
+plt.show()
+
+# p. 75 of PDF
+
+mentions = [500, 505]
+years = [2017, 2018]
+
+plt.bar(years, mentions, 0.8)
+plt.xticks(years)
+plt.ylabel("# of times I heard someone say 'data science'")
+
+# if you don't do this, matplotlib will label the x-axis 0, 1
+# and then add +2.013e3 off in the corner (bad matplotlib!)
+plt.ticklabel_format(useOffset=False)
+
+# misleading y-axis only shows the part above 500
+# plt.axis((2016.5, 2018.5, 499.0, 506.0))
+# plt.title("Look at the 'Huge' Increase!")
+# plt.show()
+
+plt.axis((2016.5, 2018.5, 0, 550))
+plt.title("Not So Huge Anymore")
+plt.show()
+
+# pp. 76-77 of PDF
+
+variance = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+bias_squared = [256, 128, 64, 32, 16, 8, 4, 2, 1]
+total_error = [x + y for x, y in zip(variance, bias_squared)]
+xs = [i for i, _ in enumerate(variance)]
+
+# We can make multiple calls to plt.plot
+# to show multiple series on the same chart
+plt.plot(xs, variance,       'g-', label='Variance')       # green solid line
+plt.plot(xs, bias_squared,   'r-.', label='Bias-Squared')  # red dot-dashed line
+plt.plot(xs, total_error,    'b:', label='Total Error')    # blue dotted line
+
+plt.legend(loc=9)
+plt.xlabel("model complexity")
+plt.xticks([])
+plt.title("The Bias-Variance Tradeoff")
+plt.show()
+
+# p. 78 of PDF
+friends = [ 70,  65,  72,  63,  71,  64,  60,  64,  67]  # noqa
+minutes = [175, 170, 205, 120, 220, 130, 105, 145, 190]
+labels  = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']  # noqa
+
+plt.scatter(friends, minutes)
+
+# label each point
+for label, friend_count, minute_count in zip(labels, friends, minutes):
+    plt.annotate(label,
+                 xy=(friend_count, minute_count),
+                 xytext=(5, -5),
+                 textcoords='offset points')
+
+plt.xlabel("# of friends")
+plt.ylabel("daily minutes spent on the site")
+plt.show()
+
+# p. 79 of PDF
+
+test_1_grades = [ 99, 90, 85, 97, 80]  # noqa
+test_2_grades = [100, 85, 60, 90, 70]
+
+plt.scatter(test_1_grades, test_2_grades)
+plt.title("Axes Aren't Comparable")
+plt.xlabel("test 1 grade")
+plt.ylabel("test 2 grade")
+plt.axis("equal")
+plt.show()
