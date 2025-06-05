de3c6eb161ed2f26083456a0da96c3916d38860d
Brian Hill <brian@lingerhere.org>
2025-06-05 14:22:06 -0700

    Finished putting the Ch. 4 assert into unit tests.

--- grus/ch04/grus_ch04_test.py
+++ grus/ch04/grus_ch04_test.py
@@ -20,7 +20,49 @@ from grus_ch04_code import *
 # assert dot([1, 2, 3], [4, 5, 6]) == 32
 # assert magnitude([3, 4]) == 5
 # assert distance([0, 1, 2, 3, 4, 5], [1, 2, 5, 6, 6, 4]) == 5
-#
+
+# This class has a test for each of Grus's vector function asserts
+class TestVectorFunctions(unittest.TestCase):
+
+    def test_add(self):
+        """Test add with two vectors."""
+        vectors = [[1, 2, 3], [4, 5, 6]]
+        result = add(vectors[0], vectors[1])
+        self.assertEqual([5, 7, 9], result)
+
+    def test_subtract(self):
+        """Test add with two vectors."""
+        vectors = [[5, 7, 9], [4, 5, 6]]
+        result = subtract(vectors[0], vectors[1])
+        self.assertEqual(result, [1, 2, 3])
+
+    def test_vector_sum(self):
+        vectors = [[1, 2], [3, 4], [5, 6], [7, 8]]
+        result = vector_sum(vectors)
+        self.assertEqual([16, 20], result)
+
+    def test_scalar_multiply(self):
+        scalar_multiply(2, [1, 2, 3])
+        self.assertEqual([2, 4, 6], scalar_multiply(2, [1, 2, 3]))
+
+    def test_vector_mean(self):
+        vectors = [[1, 2, 3], [3, 4, 1]]
+        result = vector_mean(vectors)
+        self.assertEqual([2, 3, 2], result)
+
+    def test_dot(self):
+        vectors = [1, 2, 3], [4, 5, 6]
+        result = dot(vectors[0], vectors[1])
+        self.assertEqual(32, result)
+
+    def test_magnitude(self):
+        result = magnitude([3, 4])
+        self.assertEqual(5, result)
+
+    def test_distance(self):
+        result = distance([0, 1, 2, 3, 4, 5], [1, 2, 5, 6, 6, 4])
+        self.assertEqual(5, result)
+
 # # Matrix operations
 # a = [[1, 2, 3], [4, 5, 6]]    # 2 rows and 3 columns
 # b = [[1, 2], [3, 4], [5, 6]]  # 3 rows and 2 columns
@@ -33,25 +75,29 @@ from grus_ch04_code import *
 #                               [0, 0, 0, 1, 0],
 #                               [0, 0, 0, 0, 1]]
 
-
-# The following two classes are intended to replace all the asserts above
-
-# This class needs to have a test for each of the vector functions
-class TestVectorFunctions(unittest.TestCase):
-    def test_add(self):
-        """Test add with two vectors."""
-        vectors = [[1, 2, 3], [4, 5, 6]]
-        result = add(vectors[0], vectors[1])
-        self.assertEqual([5, 7, 9], result)
-
-# This class needs to have a test for each of the matrix functions
+# This class has a test for each Grus's matrix function asserts
 class TestMatrixFunctions(unittest.TestCase):
     def test_shape(self):
         """Test vector_sum with a list of valid vectors."""
         a = [[1, 2, 3], [4, 5, 6]]    # 2 rows and 3 columns
-        assert shape(a) == (2, 3)
         result = shape(a)
         self.assertEqual((2, 3), result)
+
+    def test_get_first_column(self):
+        b = [[1, 2], [3, 4], [5, 6]]
+        self.assertEqual([1, 3, 5], get_column(b, 0))
+
+    def test_get_second_column(self):
+        b = [[1, 2], [3, 4], [5, 6]]
+        self.assertEqual([2, 4, 6], get_column(b, 1))
+
+    def test_identity_matrix(self):
+        self.assertEqual([[1, 0, 0, 0, 0],
+                          [0, 1, 0, 0, 0],
+                          [0, 0, 1, 0, 0],
+                          [0, 0, 0, 1, 0],
+                          [0, 0, 0, 0, 1]],
+                         identity_matrix(5))
 
 if __name__ == '__main__':
     unittest.main()
