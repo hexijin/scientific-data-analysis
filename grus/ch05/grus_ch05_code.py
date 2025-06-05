bbb5263ca805f6b382783cb64c148a0a14a15f16
Brian Hill <brian@lingerhere.org>
2025-06-05 09:16:07 -0700

    Ch. 5 now references Ch. 4.

---
+++ grus/ch05/grus_ch05_code.py
@@ -0,0 +1,6 @@
+# To get the following import to work, you have to add a
+# "Content Root" to the ch05 project that has the directory
+# where the ch04 project code is
+from grus_ch04_code import *
+
+assert [5, 7, 9] == add([1, 2, 3], [4, 5, 6])
