73315d7b5d2b0df54e621b0b3b94e0377864f45e
Brian Hill <brian@lingerhere.org>
2025-06-11 10:49:30 -0700

    Found the mistake that was causing a bunch of my Chapter 7 code to not work.

--- grus/grus_ch07_examples.py
+++ grus/grus_ch07_examples.py
@@ -6,35 +6,39 @@ from grus_ch07_code import *
 
 mu_0, sigma_0 = normal_approximation_to_binomial(1000, 0.5)
 
-# I am getting 463, 658, but Grus says we should get 469, 531
+# Grus says we should get 469, 531
 lower_bound, upper_bound = normal_two_sided_bounds(0.95, mu_0, sigma_0)
+print(f'Lower bound: {lower_bound}, upper bound: {upper_bound}')
 
 # PDF bottom of p. 126
 
+# 95% bounds based on assumption p is 0.5
 lo, hi = normal_two_sided_bounds(0.95, mu_0, sigma_0)
-
-print(f'Lower bound: {lower_bound}, upper bound: {upper_bound}')
 
 # The mu and sigma based on p = 0.55
 mu_1, sigma_1 = normal_approximation_to_binomial(1000, 0.55)
 
-print(f'Mean for p=0.55: {mu_1}, sigma: {sigma_1}')
-
 # A type 2 error means we fail to reject the null hypothesis,
 # which will happen when X is still in our original interval
 type_2_probability = normal_probability_between(lo, hi, mu_1, sigma_1)
-power = 1 - type_2_probability    # 0.887
 
+# Grus says we should get 0.887
+power = 1 - type_2_probability
 print(f'Power: {power}')
 
+# Grus says we should get 526
 hi = normal_upper_bound(0.95, mu_0, sigma_0)
+print(f'hi: {hi}')
 
 type_2_probability = normal_probability_below(hi, mu_1, sigma_1)
-power = 1 - type_2_probability    # 0.936
+power = 1 - type_2_probability
+# Grus says we should get 0.936
+print(f'Power: {power}')
 
 # PDF pp. 127-128
 
-two_sided_p_value(529.5, mu_0, sigma_0)   # 0.062
+# Grus says we should get 0.062
+print(f'Should get 0.062 {two_sided_p_value(529.5, mu_0, sigma_0)}')
 
 extreme_value_count = 0
 for _ in range(1000):
@@ -42,7 +46,8 @@ for _ in range(1000):
     if num_heads >= 530 or num_heads <= 470:
         extreme_value_count += 1
 
-print(f'Extreme value count: {extreme_value_count}')
+# Grus is making much too tight an assertion for this one
+print(f'Extreme value count (95% of time within 62 +- 16 (two sigma): {extreme_value_count}')
 
 upper_p_value = normal_probability_above
 lower_p_value = normal_probability_below
@@ -58,6 +63,8 @@ p525 = 525 / 1000
 mu525 = p525
 sigma525 = math.sqrt(p525 * (1 - p525) / 1000)   # 0.0158
 print(f'mu for 525: {mu525}, sigma for 525: {sigma525}')
+
+# Grus says we should get (0.4940, 0.5560)
 n525 = normal_two_sided_bounds(0.95, mu525, sigma525)
 print(f'normal_two_sided_bounds for 525: {n525}')
 
@@ -65,6 +72,8 @@ p540 = 540 / 1000
 mu540 = p540
 sigma540 = math.sqrt(p540 * (1 - p540) / 1000)   # 0.0158
 print(f'mu for 540: {mu540}, sigma for 540: {sigma540}')
+
+# Grus says we should get (0.5091, 0.5709)
 n540 = normal_two_sided_bounds(0.95, mu540, sigma540)
 print(f'normal_two_sided_bounds for 540: {n540}')
 
@@ -87,10 +96,14 @@ print(f'Number of rejections: {num_rejections} was expected to be 46')
 
 # PDF p. 134
 
-z = a_b_test_statistic(1000, 200, 1000, 180)   # -1.14
+# Grus says we should get -1.14
+z = a_b_test_statistic(1000, 200, 1000, 180)
 print(f'z = {z}')
-print(f'two_sided_p_value(z)', two_sided_p_value(z))   # 0.254
 
-z = a_b_test_statistic(1000, 200, 1000, 150)   # -2.94
+# Grus says we should get 0.254
+print(f'two_sided_p_value(z)', two_sided_p_value(z))
+
+# Grus says we should get -2.94 and 0.003
+z = a_b_test_statistic(1000, 200, 1000, 150)
 print(f'z = {z}')
-print(f'two_sided_p_value(z)', two_sided_p_value(z))   # 0.003
+print(f'two_sided_p_value(z)', two_sided_p_value(z))
