3d0e4dc5c743d9462346ba4b0747bce523ef2b5c
Brian Hill <brian@lingerhere.org>
2025-06-13 22:45:02 -0700

    Continuing Chapter 10 (now at p. 179).

--- grus/grus_ch10_code.py
+++ grus/grus_ch10_code.py
@@ -1,7 +1,9 @@
+from random import random
 from typing import List, Dict
 from collections import Counter
 import math
 import matplotlib.pyplot as plt
+from grus_ch06_code import inverse_normal_cdf
 
 # PDF p. 174
 
@@ -18,3 +20,11 @@ def plot_histogram(points: List[float], bucket_size: float, title: str = "") -> 
     plt.bar(histogram.keys(), histogram.values(), width=bucket_size)
     plt.title(title)
     plt.show()
+
+# PDF p. 176
+
+def random_normal() -> float:
+    """Returns a random draw from a standard normal distribution"""
+    return inverse_normal_cdf(random())
+
+# PDF p. 178
--- grus/grus_ch10_examples.py
+++ grus/grus_ch10_examples.py
@@ -1,6 +1,10 @@
 import random
+
+from matplotlib import pyplot as plt
+
+from grus_ch05_code import correlation
 from grus_ch06_code import inverse_normal_cdf
-from grus_ch10_code import plot_histogram
+from grus_ch10_code import plot_histogram, random_normal
 
 # PDF p. 175
 
@@ -15,4 +19,23 @@ normal = [57 * inverse_normal_cdf(random.random()) for _ in range(10000)]
 plot_histogram(uniform, 10, "Uniform Histogram")
 plot_histogram(normal, 10, "Normal Histogram")
 
-# PDF p. 175
+# PDF p. 177
+
+random.seed(0)
+xs = [random_normal() for _ in range(1000)]
+ys1 = [x + random_normal() / 2 for x in xs]
+ys2 = [-x + random_normal() / 2 for x in xs]
+
+plot_histogram(ys1, 0.01, "Normal Histogram for ys1 with bucket_size=0.01")
+plot_histogram(ys2, 0.01, "Normal Histogram for ys2 with bucket_size=0.01")
+
+plt.scatter(xs, ys1, marker='.', color='black', label='ys1')
+plt.scatter(xs, ys2, marker='.', color='gray', label='ys2')
+plt.xlabel('xs')
+plt.ylabel('ys')
+plt.legend(loc=9)
+plt.title('Very Different Joint Distributions')
+plt.show()
+
+print(correlation(xs, ys1))    # about 0.9
+print(correlation(xs, ys2))    # about -0.9
