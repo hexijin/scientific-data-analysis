6f14a15f156bec2f8e1c30db2e8cb6af64810842
Brian Hill <brian@lingerhere.org>
2025-06-22 13:47:19 -0700

    Almost done with Chapter 18 for June 23 meeting.

--- grus/grus_ch18_code.py
+++ grus/grus_ch18_code.py
@@ -1,3 +1,4 @@
+from typing import List
 import math
 
 from grus_ch04_code import Vector, dot
@@ -20,3 +21,71 @@ def neuron_output(weights: Vector, inputs: Vector) -> float:
     return sigmoid(dot(weights, inputs))
 
 # PDF p. 297
+
+def feed_forward(neural_network: List[List[Vector]], input_vector: Vector) -> List[Vector]:
+    """
+    Perhaps for debugging reasons(?), Grus is assembling the outputs of all
+    the layers, not just the last one.
+    """
+    outputs: List[Vector] = []
+
+    for layer in neural_network:
+        input_with_bias = input_vector + [1]
+        layer_output = []
+        for neuron in layer:
+            single_neuron_output = neuron_output(neuron, input_with_bias)
+            layer_output.append(single_neuron_output)
+        outputs.append(layer_output)
+        input_vector = layer_output
+
+    return outputs
+
+# PDF p. 299
+
+FIRST_LAYER = 0
+LAST_LAYER = -1
+
+def minus_sqerror_gradients(network: List[List[Vector]],
+                            input_vector: Vector,
+                            target_vector) -> List[List[Vector]]:
+    hidden_outputs, outputs = feed_forward(network, input_vector)
+    output_deltas = [output_i * (1 - output_i) * (output_i - target)
+                     for output_i, target in zip(outputs, target_vector)]
+    minus_output_grads = [[-1.0 * output_deltas[i] * hidden_output_i
+                           for hidden_output_i in hidden_outputs + [1]]
+                          for i, _ in enumerate(network[LAST_LAYER])]
+    hidden_deltas = [hidden_output_i * (1 - hidden_output_i) *
+                     dot(output_deltas, [n[i] for n in network[LAST_LAYER]])
+                     for i, hidden_output_i in enumerate(hidden_outputs)]
+    minus_hidden_grads = [[-1.0 * hidden_deltas[i] * input_i
+                           for input_i in input_vector + [1]]
+                          for i, _ in enumerate(network[FIRST_LAYER])]
+    return [minus_hidden_grads, minus_output_grads]
+
+# PDF p. 302 - Fizz Buzz
+
+NUMBER = [1, 0, 0, 0]
+FIZZ = [0, 1, 0, 0]
+BUZZ = [0, 0, 1, 0]
+# I wonder what would happen if we had one less output and made FIZZBUZZ = [0, 1, 1]
+FIZZBUZZ = [0, 0, 0, 1]
+
+def fizz_buzz_encode(x: int) -> Vector:
+    if x % 15 == 0:
+        return FIZZBUZZ
+    elif x % 5 == 0:
+        return BUZZ
+    elif x % 3 == 0:
+        return FIZZ
+    else:
+        return NUMBER
+
+BINARY_ENCODING_LENGTH = 10
+FIZZBUZZ_ENCODING_LENGTH = 4
+
+def binary_encode(x: int) -> Vector:
+    binary: Vector = []
+    for i in range(BINARY_ENCODING_LENGTH):
+        binary.append(x % 2)
+        x = x // 2
+    return binary
--- grus/grus_ch18_examples.py
+++ grus/grus_ch18_examples.py
@@ -1,4 +1,11 @@
-from grus_ch18_code import perceptron_output
+import random
+import tqdm
+
+from grus_ch04_code import squared_distance
+from grus_ch08_code import gradient_step
+from grus_ch18_code import perceptron_output, feed_forward, LAST_LAYER, minus_sqerror_gradients, binary_encode
+from grus_ch18_code import fizz_buzz_encode, NUMBER, FIZZ, BUZZ, FIZZBUZZ
+from grus_ch18_code import BINARY_ENCODING_LENGTH, FIZZBUZZ_ENCODING_LENGTH
 
 # PDF p. 293
 
@@ -24,9 +31,94 @@ not_bias = 1.0
 assert perceptron_output(not_weights, not_bias, [1]) == 0
 assert perceptron_output(not_weights, not_bias, [0]) == 1
 
-and_gate = min
-or_gate = max
-def xor_gate(x: float, y: float) -> int:
-    return 0 if x == y else 1
+# and_gate = min
+# or_gate = max
+# def xor_gate(x: float, y: float) -> int:
+#     return 0 if x == y else 1
 
 # PDF p. 297
+
+xor_network = [
+    [[20.0, 20.0, -30.0], [20.0, 20.0, -10.0]],  # The first (input) layer
+    [[-60.0, 60.0, -30.0]]                       # The last (output) layer
+]
+
+ONLY_OUTPUT = 0
+
+assert 0.000 < feed_forward(xor_network, [0, 0])[LAST_LAYER][ONLY_OUTPUT] < 0.001
+assert 0.999 < feed_forward(xor_network, [1, 0])[LAST_LAYER][ONLY_OUTPUT] < 1.000
+assert 0.999 < feed_forward(xor_network, [0, 1])[LAST_LAYER][ONLY_OUTPUT] < 1.000
+assert 0.000 < feed_forward(xor_network, [1, 1])[LAST_LAYER][ONLY_OUTPUT] < 0.001
+
+# PDF p. 300
+
+random.seed(0)
+
+fb_xs = [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]]
+fb_ys = [[0.0], [1.0], [1.0], [0.0]]
+
+# start with random weights
+test_network = [
+    # The first (input) layer
+    [[random.random() for _ in range(2 + 1)], [random.random() for _ in range(2 + 1)]],
+    # The last (output) layer
+    [[random.random() for _ in range(2 + 1)]]
+]
+
+xor_learning_rate = 1.0
+
+for _ in range(20000):
+    for training_x, training_y in zip(fb_xs, fb_ys):
+        minus_gradients = minus_sqerror_gradients(test_network, training_x, training_y)
+        test_network = [
+            [gradient_step(neuron, minus_gradient, xor_learning_rate)
+             for neuron, minus_gradient in zip(layer, minus_layer_grad)]
+            for layer, minus_layer_grad in zip(test_network, minus_gradients)
+        ]
+
+assert feed_forward(test_network, [0, 0])[LAST_LAYER][ONLY_OUTPUT] < 0.01
+assert feed_forward(test_network, [0, 1])[LAST_LAYER][ONLY_OUTPUT] > 0.99
+assert feed_forward(test_network, [1, 0])[LAST_LAYER][ONLY_OUTPUT] > 0.99
+assert feed_forward(test_network, [1, 1])[LAST_LAYER][ONLY_OUTPUT] < 0.01
+
+# PDF p. 302 - Fizz Buzz
+
+assert fizz_buzz_encode(2) == NUMBER
+assert fizz_buzz_encode(6) == FIZZ
+assert fizz_buzz_encode(10) == BUZZ
+assert fizz_buzz_encode(30) == FIZZBUZZ
+
+assert binary_encode(0) == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+assert binary_encode(1) == [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+assert binary_encode(10) == [0, 1, 0, 1, 0, 0, 0, 0, 0, 0]
+assert binary_encode(101) == [1, 0, 1, 0, 0, 1, 1, 0, 0, 0]
+assert binary_encode(999) == [1, 1, 1, 0, 0, 1, 1, 1, 1, 1]
+
+fb_range = range(101, 1024)
+fb_xs = [binary_encode(n) for n in fb_range]
+fb_ys = [fizz_buzz_encode(n) for n in fb_range]
+
+NUM_HIDDEN = 25
+
+fb_network = [
+    [[random.random() for _ in range(BINARY_ENCODING_LENGTH + 1)] for _ in range(NUM_HIDDEN)],
+    [[random.random() for _ in range(NUM_HIDDEN + 1)] for _ in range(FIZZBUZZ_ENCODING_LENGTH)]
+]
+
+fb_learning_rate = 1.0
+
+with tqdm.trange(500) as t:
+    for epoch in t:
+        epoch_loss = 0.0
+        for fb_x, fb_y in zip(fb_xs, fb_ys):
+            predicted = feed_forward(fb_network, fb_x)[LAST_LAYER]
+            epoch_loss += squared_distance(predicted, fb_y)
+            minus_gradients = minus_sqerror_gradients(fb_network, fb_x, fb_y)
+            # Take a gradient step for each neuron in each layer
+            fb_network = [
+                [gradient_step(neuron, minus_grad, fb_learning_rate)
+                 for neuron, minus_grad in zip(layer, minus_layer_grad)]
+                for layer, minus_layer_grad in zip(fb_network, minus_gradients)
+            ]
+        t.set_description(f"fizz buzz (loss: {epoch_loss:.2f})")
+
